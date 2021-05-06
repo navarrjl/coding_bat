82f150d781c9fce5e70c45a920041b1bd1c8846f
navarrjl <77976844+navarrjl@users.noreply.github.com>
2021-05-06 11:15:04 -0400

    Update ns.py

---
+++ warmup-1/not_string/ns.py
@@ -0,0 +1,11 @@
+'''
+https://codingbat.com/prob/p189441
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins 
+with "not", return the string unchanged.
+'''
+def not_string(str):
+  if len(str) >= 3 and str[:3] == "not":
+    return str
+  return "not " + str
+  # str[:3] goes from the start of the string up to but not
+  # including index 3
